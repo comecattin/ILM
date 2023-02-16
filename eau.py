e03063a68779ada089508fd40ad31dd9ef2cf799
Côme Cattin <75748278+sequoia-dev@users.noreply.github.com>
2023-02-16 16:25:13 +0100

    Formated the code.

--- eau.py
+++ eau.py
@@ -8,6 +8,7 @@ import numpy as np
 import extract_gmx_energy as xtract
 import os
 
+
 class eau(xtract.gromacs_output):
     """
     Class of the different water model (OPC and TIP3P studied)
@@ -28,29 +29,29 @@ class eau(xtract.gromacs_output):
         volume_error : float
             Volume associated error
         """
-        #Avogadro constant
+        # Avogadro constant
         Avogadro = 6.0221408e23
-        #Molar mass of water
+        # Molar mass of water
         molar_mass = 18.01528e-3
 
         x, density = self.extract()
-        
-        #Mecular volume in m^3/molecule
-        volume = molar_mass/(density*Avogadro)
-        #Convert to Angstrom^3
-        volume = volume*1e30
-        
-        #Compute the mean and the error
+
+        # Mecular volume in m^3/molecule
+        volume = molar_mass / (density * Avogadro)
+        # Convert to Angstrom^3
+        volume = volume * 1e30
+
+        # Compute the mean and the error
         density_mean_error = np.array(self.analyze())
         density_mean = density_mean_error[0]
-        density_error  = density_mean_error[1]
+        density_error = density_mean_error[1]
 
-        volume_mean = molar_mass/(density_mean*Avogadro)*1e30
-        volume_error = molar_mass/(density_mean**2 *Avogadro)*1e30
+        volume_mean = molar_mass / (density_mean * Avogadro) * 1e30
+        volume_error = molar_mass / (density_mean**2 * Avogadro) * 1e30
 
         return x, volume, volume_mean, volume_error
-    
-    def plot_volume(self, xlabel, ylabel, output_name,color="b",show=True):
+
+    def plot_volume(self, xlabel, ylabel, output_name, color="b", show=True):
         """
         Plot the molecular volume as a function of time
 
@@ -70,11 +71,11 @@ class eau(xtract.gromacs_output):
         x, volume, volume_mean, volume_error = self.density_to_volume()
         fig, ax = plt.subplots()
         ax.plot(x, volume, color=color, label="Molecular volume")
-        ax.plot(x, np.ones(len(x))*volume_mean,linewidth=5, label="Mean")
+        ax.plot(x, np.ones(len(x)) * volume_mean, linewidth=5, label="Mean")
         ax.set_xlabel(xlabel)
         ax.set_ylabel(ylabel)
         ax.legend()
-        plt.savefig(output_name, format="pdf", dpi=300, bbox_inches='tight')
+        plt.savefig(output_name, format="pdf", dpi=300, bbox_inches="tight")
         if show:
             plt.show()
 
@@ -86,17 +87,17 @@ class eau(xtract.gromacs_output):
         file_name : str
             Path where to save the file
         """
-        #Compute results
+        # Compute results
         x, volume, volume_mean, volume_error = self.density_to_volume()
-        #Save the results
-        #The first line contain the mean volume and the estimated error
-        #The line after that contains the time and the volume
+        # Save the results
+        # The first line contain the mean volume and the estimated error
+        # The line after that contains the time and the volume
         np.savetxt(
             file_name,
-            np.array([x,volume]),
-            header="{} {}".format(volume_mean,volume_error)
-            )
-    
+            np.array([x, volume]),
+            header="{} {}".format(volume_mean, volume_error),
+        )
+
     def read(self, file_name):
         """Read the data from .txt file
 
@@ -120,20 +121,22 @@ class eau(xtract.gromacs_output):
         x, volume = np.loadtxt(file_name)
         with open(file_name) as f:
             line = f.readline()
-        line = line.replace("#","").replace("\n","").split(" ")[1:]
+        line = line.replace("#", "").replace("\n", "").split(" ")[1:]
         volume_mean = float(line[0])
         volume_error = float(line[1])
 
         return x, volume, volume_mean, volume_error
 
-    def cutoff_extract_volume(self,
-                            dir="/home/ccattin/Documents/EAU/Change_cutoff/TIP3P/300K/"):
+    def cutoff_extract_volume(
+        self, dir="/home/ccattin/Documents/EAU/Change_cutoff/TIP3P/300K/"
+    ):
         """Extract the volume and the associated cut-off from simulations
 
         Parameters
         ----------
         dir : str, optional
-            Directory where the pipeline has been run, by default "/home/ccattin/Documents/EAU/Change_cutoff/TIP3P/300K/"
+            Directory where the pipeline has been run,
+            by default "/home/ccattin/Documents/EAU/Change_cutoff/TIP3P/300K/"
 
         Returns
         -------
@@ -144,28 +147,28 @@ class eau(xtract.gromacs_output):
         volume_error_list : np.array
             Array containing the error associated
         """
-        #Initialize
+        # Initialize
         volume_mean_list = []
         volume_error_list = []
         cutoff_list = []
-        #Get the cutoff directories name
+        # Get the cutoff directories name
         for name in os.listdir(dir):
-            #Ignore other directories
+            # Ignore other directories
             if "cutoff_" in name:
-                cutoff = float(name.replace("cutoff_",""))
+                cutoff = float(name.replace("cutoff_", ""))
                 cutoff_list.append(cutoff)
-                #Extract values
-                (x, volume, 
-                volume_mean, 
-                volume_error) = self.read(
-                                    dir+name+"/analysis/time_volume_mean_error.txt"
-                                    )
+                # Extract values
+                (x, volume, volume_mean, volume_error) = self.read(
+                    dir + name + "/analysis/time_volume_mean_error.txt"
+                )
                 volume_mean_list.append(volume_mean)
                 volume_error_list.append(volume_error)
-        
-        return (np.array(cutoff_list),
-                np.array(volume_mean_list),
-                np.array(volume_error_list))
+
+        return (
+            np.array(cutoff_list),
+            np.array(volume_mean_list),
+            np.array(volume_error_list),
+        )
 
     def cutoff_plot(self, cutoff_dir, color="b", show=True, output_path="cutoff.pdf"):
         """Plot the volume as a function of the used cut-off.
@@ -181,32 +184,36 @@ class eau(xtract.gromacs_output):
         output_path : str, optional
             Path to save the file, by default "cutoff.pdf"
         """
-        #Extract data
-        (cutoff_list, 
-            volume_mean_list, 
-            volume_error_list) = self.cutoff_extract_volume(cutoff_dir)
+        # Extract data
+        (cutoff_list, volume_mean_list, volume_error_list) = self.cutoff_extract_volume(
+            cutoff_dir
+        )
         fig, ax = plt.subplots()
-        ax.errorbar(cutoff_list,volume_mean_list,
-                    volume_error_list,fmt=".",
-                    color=color)
+        ax.errorbar(
+            cutoff_list, volume_mean_list, volume_error_list, fmt=".", color=color
+        )
         ax.grid()
         ax.set_xlabel("Cut-off (nm)")
         ax.set_ylabel(r"Molecular volume ($\AA^{3}$.molec$^{-1}$)")
 
-        plt.savefig(output_path, dpi=300, bbox_inches='tight')
-        
-        #Plot in an external window
+        plt.savefig(output_path, dpi=300, bbox_inches="tight")
+
+        # Plot in an external window
         if show:
             plt.show()
 
-    def simulation_length_extract_volume(self,
-                            dir="/home/ccattin/Documents/EAU/Change_simulatio_length/TIP3P/300K/"):
-        """Extract the volume and the associated simulation length from simulations
+    def simulation_length_extract_volume(
+        self, dir="/home/ccattin/Documents/EAU/Change_simulatio_length/TIP3P/300K/"
+    ):
+        """
+        Extract the volume and the associated simulation length.
 
         Parameters
         ----------
         dir : str, optional
-            Directory where the pipeline has been run, by default "/home/ccattin/Documents/EAU/Change_simulatio_length/TIP3P/300K/"
+            Directory where the pipeline has been run,
+            by default
+            "/home/ccattin/Documents/EAU/Change_simulatio_length/TIP3P/300K/"
 
         Returns
         -------
@@ -217,34 +224,36 @@ class eau(xtract.gromacs_output):
         volume_error_list : np.array
             Array containing the error associated
         """
-        #Initialize
+        # Initialize
         volume_mean_list = []
         volume_error_list = []
         nsteps_list = []
-        #Get the nsteps directories name
+        # Get the nsteps directories name
         for name in os.listdir(dir):
-            #Ignore other directories
+            # Ignore other directories
             if "nsteps_" in name:
-                nsteps = float(name.replace("nsteps_",""))
+                nsteps = float(name.replace("nsteps_", ""))
                 nsteps_list.append(nsteps)
-                #Extract values
-                (x, volume, 
-                volume_mean, 
-                volume_error) = self.read(
-                                    dir+name+"/analysis/time_volume_mean_error.txt"
-                                    )
+                # Extract values
+                (x, volume, volume_mean, volume_error) = self.read(
+                    dir + name + "/analysis/time_volume_mean_error.txt"
+                )
                 volume_mean_list.append(volume_mean)
                 volume_error_list.append(volume_error)
-        
-        return (np.array(nsteps_list),
-                np.array(volume_mean_list),
-                np.array(volume_error_list))
-        
-    def simulation_length_plot(self,
-                    simulation_length_dir,
-                    color="b",
-                    show=True,
-                    output_path="simulation_length.pdf"):
+
+        return (
+            np.array(nsteps_list),
+            np.array(volume_mean_list),
+            np.array(volume_error_list),
+        )
+
+    def simulation_length_plot(
+        self,
+        simulation_length_dir,
+        color="b",
+        show=True,
+        output_path="simulation_length.pdf",
+    ):
         """Plot the volume as a function of the used nsteps.
 
         Parameters
@@ -258,24 +267,28 @@ class eau(xtract.gromacs_output):
         output_path : str, optional
             Path to save the file, by default "simulation_length.pdf"
         """
-        #Extract data
-        (simulation_length_list, 
-            volume_mean_list, 
-            volume_error_list) = self.simulation_length_extract_volume(simulation_length_dir)
+        # Extract data
+        (
+            simulation_length_list,
+            volume_mean_list,
+            volume_error_list,
+        ) = self.simulation_length_extract_volume(simulation_length_dir)
         fig, ax = plt.subplots()
-        ax.errorbar(simulation_length_list,volume_mean_list,
-                    volume_error_list,fmt=".",
-                    color=color)
+        ax.errorbar(
+            simulation_length_list,
+            volume_mean_list,
+            volume_error_list,
+            fmt=".",
+            color=color,
+        )
         ax.grid()
         ax.set_xlabel("nsteps")
         ax.set_ylabel(r"Molecular volume ($\AA^{3}$.molec$^{-1}$)")
 
-        plt.savefig(output_path, dpi=300, bbox_inches='tight')
-        #Plot in an external window
+        plt.savefig(output_path, dpi=300, bbox_inches="tight")
+        # Plot in an external window
         if show:
             plt.show()
-
-
 
 
 if __name__ == "__main__":
@@ -285,7 +298,9 @@ if __name__ == "__main__":
     xlabel = "Time (ps)"
     ylabel = r"Molecular volume ($\AA^{3}$.molec$^{-1}$)"
     output_name = "/home/ccattin/Documents/Code/outputs/molar_volume.pdf"
-    output_result_name = "/home/ccattin/Documents/Code/outputs/time_volume_mean_error.txt"
+    output_result_name = (
+        "/home/ccattin/Documents/Code/outputs/time_volume_mean_error.txt"
+    )
     color = sns.color_palette("cool", 12)[6]
     show = True
     #######
@@ -297,32 +312,40 @@ if __name__ == "__main__":
     print(
         """
         Mean molecular volume : {} +/- {} Angstrom/molec
-        """.format(volume_mean, volume_error))
-    
-    OPC.plot_volume(xlabel=xlabel, ylabel=ylabel, 
-                    output_name=output_name, color=color,
-                    show=show)
+        """.format(
+            volume_mean, volume_error
+        )
+    )
+
+    OPC.plot_volume(
+        xlabel=xlabel, ylabel=ylabel, output_name=output_name, color=color, show=show
+    )
 
     OPC.write(output_result_name)
-    (x, volume,
-    volume_mean, volume_error) = OPC.read(output_result_name)
-    
+    (x, volume, volume_mean, volume_error) = OPC.read(output_result_name)
+
     ##########
     # Cutoff #
     ##########
     cutoff_dir = "/home/ccattin/Documents/EAU/Change_cutoff/TIP3P/300K/"
-    output_path="/home/ccattin/Documents/Code/outputs/cutoff.pdf"
-    (cutoff_list,
-        volume_mean_list,
-        volume_error_list) = OPC.cutoff_extract_volume(cutoff_dir)
-    OPC.cutoff_plot(cutoff_dir,color,show=show,output_path=output_path)
+    output_path = "/home/ccattin/Documents/Code/outputs/cutoff.pdf"
+    (cutoff_list, volume_mean_list, volume_error_list) = OPC.cutoff_extract_volume(
+        cutoff_dir
+    )
+    OPC.cutoff_plot(cutoff_dir, color, show=show, output_path=output_path)
 
     #####################
     # Simulation length #
     #####################
-    simulation_length_dir = "/home/ccattin/Documents/EAU/Change_simulation_length/TIP3P/300K/"
-    output_path="/home/ccattin/Documents/Code/outputs/simulation_length.pdf"
-    (cutoff_list,
+    simulation_length_dir = (
+        "/home/ccattin/Documents/EAU/Change_simulation_length/TIP3P/300K/"
+    )
+    output_path = "/home/ccattin/Documents/Code/outputs/simulation_length.pdf"
+    (
+        cutoff_list,
         volume_mean_list,
-        volume_error_list) = OPC.simulation_length_extract_volume(simulation_length_dir)
-    OPC.simulation_length_plot(simulation_length_dir,color,show=show,output_path=output_path)
+        volume_error_list,
+    ) = OPC.simulation_length_extract_volume(simulation_length_dir)
+    OPC.simulation_length_plot(
+        simulation_length_dir, color, show=show, output_path=output_path
+    )
--- eau_CLI.py
+++ eau_CLI.py
@@ -9,167 +9,181 @@ import seaborn as sns
 import eau
 import extract_gmx_energy
 
+
 def main():
     """
     CLI main function
     """
     parser = argparse.ArgumentParser(
-        "CLI for water simulations", description="Compute the molecular volume of water."
+        "CLI for water simulations",
+        description="Compute the molecular volume of water.",
     )
-    #Analysis or not the molecular volume
+    # Analysis or not the molecular volume
     parser.add_argument(
         "--volume",
         dest="output_volume",
         help="Analysis of the molecular volume",
-        action="store_true"
+        action="store_true",
     )
-    #File Path
+    # File Path
     parser.add_argument(
         "-f",
         "--file-name",
         help="Path to the .xvg gromacs output file to extract data from",
         type=str,
-        dest="file_name"
-        )
-    #Error File path
+        dest="file_name",
+    )
+    # Error File path
     parser.add_argument(
         "-e",
         "--error-file",
         help="Path to the .xvg gromacs analyze output file estimate of the error",
         type=str,
         dest="error_file",
-        )
-    #Output the graph in an external windown or not
-    parser.add_argument(
-        "-p","--plot",
-        help="Plot the molecular volume as a function of time",
-        action="store_true"
     )
-    #Get the path to the output pdf file
+    # Output the graph in an external windown or not
+    parser.add_argument(
+        "-p",
+        "--plot",
+        help="Plot the molecular volume as a function of time",
+        action="store_true",
+    )
+    # Get the path to the output pdf file
     parser.add_argument(
         "--pdf",
         dest="output_pdf",
         help="Path where to output the .pdf file of the molar volume plot",
         type=str,
-        default="molar_volume.pdf"
+        default="molar_volume.pdf",
     )
-    #Get the path to the output txt file
+    # Get the path to the output txt file
     parser.add_argument(
         "--txt",
         dest="output_txt",
         help="Path where to output the .txt file of the results",
         type=str,
-        default="time_volume_mean_error.txt"
+        default="time_volume_mean_error.txt",
     )
-    #Extract or not the cutoff
+    # Extract or not the cutoff
     parser.add_argument(
-        "-c","--cutoff",
+        "-c",
+        "--cutoff",
         help="Extract and plot the volume as a funtion of the cutoff",
-        action="store_true"
+        action="store_true",
     )
-    #Save the .pdf from the cutoff analysis
+    # Save the .pdf from the cutoff analysis
     parser.add_argument(
         "--cutoff-pdf",
         help="Path to save the pdf file of the plot of the volume as a function of the cutoff",
         default="cutoff.pdf",
-        dest="cutoff_pdf"
+        dest="cutoff_pdf",
     )
-    #Cutoff directory
+    # Cutoff directory
     parser.add_argument(
         "--cutoff-dir",
         help="Directory where the pipeline has been run.",
         default="./",
-        dest="cutoff_dir"
+        dest="cutoff_dir",
     )
-    #Extract or not the simulation length
+    # Extract or not the simulation length
     parser.add_argument(
         "--simulation-length",
         dest="simulation_length",
         action="store_true",
-        help="Extract and plot the volume as a function of the simulation length (as a function of the number of steps)"
+        help="Extract and plot the volume as a function of the simulation length (as a function of the number of steps)",
     )
-    #Save the .pdf from the simulation length analysis
+    # Save the .pdf from the simulation length analysis
     parser.add_argument(
         "--simulation-length-pdf",
         help="Path to save the pdf file of the plot of the volume as a function of the simualtion length",
         default="simulation_length.pdf",
-        dest="simulation_length_pdf"
+        dest="simulation_length_pdf",
     )
-    #Simulation analysis directory
+    # Simulation analysis directory
     parser.add_argument(
         "--simulation-length-dir",
         help="Directory where the pipeline has been run.",
         default="./",
-        dest="simulation_length_dir"
+        dest="simulation_length_dir",
     )
     args = parser.parse_args()
 
-    #Define the output and plot properties
+    # Define the output and plot properties
     color = sns.color_palette("cool", 12)[6]
     xlabel = "Time (ps)"
     ylabel = r"Molecular volume ($\AA^{3}$.molec$^{-1}$)"
 
-    #Raise error if no options are provided
+    # Raise error if no options are provided
     no_option = (
-        args.output_volume == False and 
-        args.cutoff == False and 
-        args.simulation_length == False
-        )
+        args.output_volume == False
+        and args.cutoff == False
+        and args.simulation_length == False
+    )
     if no_option:
         raise Exception("No options provided. Please use the -h option.")
 
     else:
 
-        #Molecular Volume part
+        # Molecular Volume part
         if args.output_volume:
-            
-            #Raise error if some file are missing
+
+            # Raise error if some file are missing
             if args.file_name == None or args.error_file == None:
-                raise Exception("No file or no error file provided. Please use the -h option.")
-            
+                raise Exception(
+                    "No file or no error file provided. Please use the -h option."
+                )
+
             else:
-                #Open a water model
+                # Open a water model
                 water_model = eau.eau(args.file_name, args.error_file)
-                #Compute its volume from its density
+                # Compute its volume from its density
                 x, volume, volume_mean, volume_error = water_model.density_to_volume()
-                #Print out the result
+                # Print out the result
                 print(
                     """
                     Mean molecular volume : {} +/- {} Angstrom/molec
-                    """.format(volume_mean, volume_error)
+                    """.format(
+                        volume_mean, volume_error
                     )
-                #Plot the result
-                water_model.plot_volume(xlabel=xlabel, ylabel=ylabel, 
-                                output_name=args.output_pdf, color=color,
-                                show=args.plot)
-                #Save the result
+                )
+                # Plot the result
+                water_model.plot_volume(
+                    xlabel=xlabel,
+                    ylabel=ylabel,
+                    output_name=args.output_pdf,
+                    color=color,
+                    show=args.plot,
+                )
+                # Save the result
                 water_model.write(args.output_txt)
 
-        #Cutoff part
+        # Cutoff part
         if args.cutoff:
             water_model = eau.eau()
-            (cutoff_list,
+            (
+                cutoff_list,
                 volume_mean_list,
-                volume_error_list) = water_model.cutoff_extract_volume(
-                                                    args.cutoff_dir
-                                                    )
-            water_model.cutoff_plot(args.cutoff_dir,
-                                    color,
-                                    show=args.plot,
-                                    output_path=args.cutoff_pdf)
-        
-        #Simulation length part
+                volume_error_list,
+            ) = water_model.cutoff_extract_volume(args.cutoff_dir)
+            water_model.cutoff_plot(
+                args.cutoff_dir, color, show=args.plot, output_path=args.cutoff_pdf
+            )
+
+        # Simulation length part
         if args.simulation_length:
             water_model = eau.eau()
-            (simulation_length,
+            (
+                simulation_length,
                 volume_mean_list,
-                volume_error_list) = water_model.simulation_length_extract_volume(
-                                                    args.simulation_length_dir
-                                                    )
-            water_model.simulation_length_plot(args.simulation_length_dir,
-                                    color,
-                                    show=args.plot,
-                                    output_path=args.simulation_length_pdf)
+                volume_error_list,
+            ) = water_model.simulation_length_extract_volume(args.simulation_length_dir)
+            water_model.simulation_length_plot(
+                args.simulation_length_dir,
+                color,
+                show=args.plot,
+                output_path=args.simulation_length_pdf,
+            )
+
 
 if __name__ == "__main__":
     main()
