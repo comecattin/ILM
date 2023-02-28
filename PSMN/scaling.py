f978de41acfba428b74e2de7f7abdc1a777f83b8
Côme Cattin <75748278+sequoia-dev@users.noreply.github.com>
2023-02-28 14:32:09 +0100

    Formated with black.

--- PSMN/scaling.py
+++ PSMN/scaling.py
@@ -8,6 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 def extract_simulation_time(path):
     """Extract the wall time that one simulation takes
 
@@ -21,13 +22,14 @@ def extract_simulation_time(path):
     time : float
         Wall time of the simulation
     """
-    command = "grep Time: "+path
+    command = "grep Time: " + path
     command = command.split()
     out = str(subprocess.check_output(command))
-    out = out.replace("b'","").replace("Time:",'').replace("\\n'",'')
+    out = out.replace("b'", "").replace("Time:", "").replace("\\n'", "")
     out = out.split()
     time = float(out[1])
     return time
+
 
 def get_time(path):
     """Get the wall time of multiple simulation
@@ -44,24 +46,25 @@ def get_time(path):
     time_list : list
         List containing the corresponding wall time
     """
-    #Init
+    # Init
     cores_list = []
     time_list = []
-    #Search inside directory
+    # Search inside directory
     for name in os.listdir(path):
-        #Take only directories with cores in their name
-        if 'cores_' in name:
+        # Take only directories with cores in their name
+        if "cores_" in name:
             cores = int(name.replace("cores_", ""))
-            #Get the number of cores
+            # Get the number of cores
             cores_list.append(cores)
-            #Production log file
-            log_path = path+name+'/production/prod.log'
-            #Get the wall time
+            # Production log file
+            log_path = path + name + "/production/prod.log"
+            # Get the wall time
             time = extract_simulation_time(log_path)
             time_list.append(time)
     return cores_list, time_list
 
-def speedup(cores_list,time_list):
+
+def speedup(cores_list, time_list):
     """Compute the speed-up
 
     Parameters
@@ -78,15 +81,16 @@ def speedup(cores_list,time_list):
     speedup : np.array
         Array containing the corresponding speedup
     """
-    #Convert to array
+    # Convert to array
     cores_list = np.array(cores_list)
     time_list = np.array(time_list)
     minimal_core_index = np.argmin(cores_list)
-    #Compute speed-up
-    speedup = time_list[minimal_core_index]/time_list
+    # Compute speed-up
+    speedup = time_list[minimal_core_index] / time_list
     return cores_list, speedup
 
-def plot_speedup(cores_list, speedup, color='b',outputname='speedup.pdf'):
+
+def plot_speedup(cores_list, speedup, color="b", outputname="speedup.pdf"):
     """Plot the speed up as a fonction of the number of nodes
 
     Parameters
@@ -101,10 +105,10 @@ def plot_speedup(cores_list, speedup, color='b',outputname='speedup.pdf'):
         Path where to save the file, by default 'speedup.pdf'
     """
     fig, ax = plt.subplots()
-    #Plot a theoritical linear curve
-    linear = cores_list/np.amin(cores_list)
-    ax.plot(cores_list,linear,'--',label="Linear law")
-    ax.scatter(cores_list,speedup,color=color)
+    # Plot a theoritical linear curve
+    linear = cores_list / np.amin(cores_list)
+    ax.plot(cores_list, linear, "--", label="Linear law")
+    ax.scatter(cores_list, speedup, color=color)
     ax.set_xticks(cores_list)
     ax.set_xlabel("Number of CPU")
     ax.set_ylabel("Speed-up")
@@ -119,5 +123,5 @@ if __name__ == "__main__":
     path = "/home/ccattin/Documents/HSP90_278K/test_parallel/"
     outputname = "/home/ccattin/Documents/HSP90_278K/test_parallel/analysis/speedup.pdf"
     cores_list, time_list = get_time(path)
-    cores_list, speedup = speedup(cores_list,time_list)
-    plot_speedup(cores_list,speedup,color,outputname)
+    cores_list, speedup = speedup(cores_list, time_list)
+    plot_speedup(cores_list, speedup, color, outputname)
