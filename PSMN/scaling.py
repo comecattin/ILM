da27b494b6e57d78438d5db065b047e62001d429
Côme Cattin <75748278+sequoia-dev@users.noreply.github.com>
2023-03-02 11:13:12 +0100

    Analysis for the scaling on different nodes.

--- PSMN/scaling.py
+++ PSMN/scaling.py
@@ -117,11 +117,55 @@ def plot_speedup(cores_list, speedup, color="b", outputname="speedup.pdf"):
     plt.savefig(outputname, format="pdf", dpi=300, bbox_inches="tight")
     plt.show()
 
+def get_CPU_node_time(path):
+    cpu = []
+    node = []
+    time = []
+    for name in os.listdir(path):
+        if 'CPU' in name:
+            list_CPU_NODE = name.replace(
+                "CPU_",""
+                ).replace(
+                "NODE_",""
+                ).replace(
+                ".log",""
+                ).split("_")
+            cpu.append(int(list_CPU_NODE[0]))
+            node.append(int(list_CPU_NODE[1]))
+            time.append(
+                extract_simulation_time(path+"/"+name)
+            )
+    return np.array(cpu), np.array(node), np.array(time)
+
+def plot_CPU_node_time(cpu,node,time,color=["b","r","g"],outputname="speedup_node.pdf"):
+    fig, ax = plt.subplots()
+    for i, node_i in enumerate((1,2,4)):
+        cpu_node_i = list(cpu[np.where(node==node_i)])
+        time_node_i = list(time[np.where(node==node_i)])
+        if node_i == 1:
+            minimal_cpu_index = np.argmin(cpu_node_i)
+            ref = time_node_i[minimal_cpu_index]
+        speedup = ref/time_node_i
+        ax.scatter(cpu_node_i,speedup,
+                   label=str(node_i)+" nodes",
+                   color=color[i])
+    ax.legend()
+    ax.grid()
+    ax.set_xticks([2,4,8,16,32,64,96,128])
+    ax.set_xlabel("Number of CPUs")
+    ax.set_ylabel("Speed-up")
+    plt.savefig(outputname, format="pdf", dpi=300, bbox_inches="tight")
+    plt.show()
 
 if __name__ == "__main__":
-    color = sns.color_palette("cool", 12)[6]
+    color_palette = sns.color_palette("cool", 12)
     path = "/home/ccattin/Documents/HSP90_278K/test_parallel/"
     outputname = "/home/ccattin/Documents/HSP90_278K/test_parallel/analysis/speedup.pdf"
     cores_list, time_list = get_time(path)
-    cores_list, speedup = speedup(cores_list, time_list)
-    plot_speedup(cores_list, speedup, color, outputname)
+    cores_list, speedup_list = speedup(cores_list, time_list)
+    #plot_speedup(cores_list, speedup, color_palette[6], outputname)
+    path_node = "/home/ccattin/Documents/HSP90_278K/test_parallel/analysis"
+    outputname="/home/ccattin/Documents/HSP90_278K/test_parallel/analysis/speedup_node.pdf"
+    cpu, node, time = get_CPU_node_time(path_node)
+    color = [color_palette[i] for i in (0,6,11)]
+    plot_CPU_node_time(cpu,node,time,color=color,outputname=outputname)
