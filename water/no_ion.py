c245d6a7a15141c5b251d4e24a5403095c227cd8
Côme Cattin <cattin.come@gmail.com>
2023-03-23 16:04:12 +0100

    Get the mean volume of the water and the volume of the total simulation

--- water/no_ion.py
+++ water/no_ion.py
@@ -5,30 +5,46 @@ Extract the volume of the water + ions and get the protein volume
 import numpy as np
 import extract_gmx_energy as xtract
 import os
+import protein_volume
 
 class no_ion:
 
-    def __init__(self,path):
-        self.path = path
+    def __init__(self,path_water,path_protein):
+        
+        self.path_water = path_water
+        self.path_protein = path_protein
+        
+        self.state_dict = {}
 
     def mean_volume(self):
         state_dict = {}
-        for name in os.listdir(self.path):
+        for name in os.listdir(self.path_water):
             state = name[5:9]
             volume_file = os.path.join(
-                self.path,name,'volume_md_11.xvg'
+                self.path_water,name,'volume_md_11.xvg'
             )
             error_file = os.path.join(
-                self.path,name,'errest_md_11.xvg'
+                self.path_water,name,'errest_md_11.xvg'
             )
             ouput = xtract.gromacs_output(
                 file_name=volume_file,
                 error_file=error_file
             )
             mean_error = ouput.analyze()
-            state_dict[state] = mean_error
+            state_dict[state] = list(mean_error)
+        self.state_dict = state_dict
         
-
+    def get_total_volume_simulation(self,concatenate=False):
+        for name in os.listdir(path_protein):
+            if name[5:9] in self.state_dict:
+                config = protein_volume.configuration(int(name[7:9]),name[5:7])
+                if concatenate:
+                    config.concatenate(self.path_protein)
+                concatenated = os.path.join(
+                    self.path_protein,name,'md_concatenated.txt'
+                    )
+                t_Vprot_mean_error = config.load_txt(concatenated)
+                self.state_dict[name[5:9]].append(t_Vprot_mean_error)
 
 
 
@@ -44,6 +60,8 @@ class no_ion:
 
 
 if __name__ == '__main__':
-    path = '/home/ccattin/Documents/EAU/NO_COUNTERIONS'
-    NoIon = no_ion(path)
+    path_water = '/home/ccattin/Documents/EAU/NO_COUNTERIONS'
+    path_protein = '/home/ccattin/Documents/EAU/HSP90_simulation'
+    NoIon = no_ion(path_water,path_protein)
     NoIon.mean_volume()
+    NoIon.get_total_volume_simulation()
