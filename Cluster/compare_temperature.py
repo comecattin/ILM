861d972af00a93d73f04125b051c0ff55991bf08
Côme Cattin <cattin.come@gmail.com>
2023-04-14 15:24:45 +0200

    Load the log file and extract the cluster data

---
+++ Cluster/compare_temperature.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+import numpy as np
+
+def load_log(log_file):
+    clusters = []
+    with open(log_file) as f:
+        for line in f.readlines():
+            if 'Frames' in line:
+                list_frame = line.replace('\n','').replace(' ','').replace('Frames','').replace(':','')
+                list_frame = [int(s) for s in list_frame.split(',')[1:-1]]
+                clusters.append(list_frame)
+    return clusters
+
+def get_dict(clusters, trajectory_lim):
+    dict_cluster = {}
+    for temperature in (278,300):
+        for id_cluster, cluster in enumerate(clusters):
+            if temperature == 278:
+                dict_cluster[(id_cluster,temperature)] = [frame for frame in cluster if frame <= trajectory_lim]
+            if temperature == 300:
+                dict_cluster[(id_cluster,temperature)] = [frame for frame in cluster if frame >= trajectory_lim]
+    return dict_cluster
+
+if __name__ == '__main__':
+    log_file = '/home/ccattin/Documents/Cluster/total/clustering/clustering.log'
+    trajectory_lim = 4001
+    clusters = load_log(log_file)
+
+    dict_cluster = get_dict(clusters, trajectory_lim)
