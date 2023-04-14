28f5aebf0826071a3f4e091c457492c32238d13c
Côme Cattin <cattin.come@gmail.com>
2023-04-14 15:54:34 +0200

    Convert the dictionnary of frames into dictionnary of number of frames

--- Cluster/compare_temperature.py
+++ Cluster/compare_temperature.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 import numpy as np
+import matplotlib.pyplot as plt
 
 def load_log(log_file):
     clusters = []
@@ -21,8 +22,25 @@ def get_dict(clusters, temperatures, trajectory_lim):
                 dict_cluster[(id_cluster,temperature)] = [frame for frame in cluster if frame >= trajectory_lim]
     return dict_cluster
 
+def get_number_frames(dict_cluster):
+    dict_cluster_number = {}
+    for key, list_of_frame in dict_cluster.items():
+        dict_cluster_number[key] = len(list_of_frame)
+    return dict_cluster_number
+
+
 def plot_barplot(dict_cluster):
-    pass
+    fig, ax = plt.subplots()
+    cluster_id = np.arange(5)
+
+    cluster_temperature = [frame for key, frame in dict_cluster.items() if key[1] == 278]
+
+    ax.plot(cluster_id,cluster_temperature)
+
+
+
+
+    plt.show()
 
 if __name__ == '__main__':
     log_file = '/home/ccattin/Documents/Cluster/total/clustering/clustering.log'
@@ -33,3 +51,7 @@ if __name__ == '__main__':
     dict_cluster = get_dict(clusters=clusters,
                             temperatures=temperatures, 
                             trajectory_lim=trajectory_lim)
+    
+    dict_cluster_number = get_number_frames(dict_cluster=dict_cluster)
+
+    #plot_barplot(dict_cluster=dict_cluster)
