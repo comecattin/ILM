0f764291c8c8faeaa6132769d529a855580482f2
Côme Cattin <cattin.come@gmail.com>
2023-05-03 12:50:38 +0200

    Handles errors

--- Markov/Lethe/LETHE.py
+++ Markov/Lethe/LETHE.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 """LETHE automatizes the MSM analysis"""
 
 import argparse
@@ -15,14 +16,29 @@ def main():
     parser.add_argument(
         '-f',
         '--files',
+        required=True,
         nargs='+',
         help='List of files'
         )
+    parser.add_argument(
+        '-d',
+        '--distances',
+        nargs='+',
+        help='List of the pair names'
+    )
     
     args = parser.parse_args()
 
+    #Handles error
+    if not args.files:
+        parser.error("No trajectories file given")
+    if not args.distances:
+        parser.error("No distances given")
+
     file_list = args.files
+    pairNames = args.distances
     print(file_list)
+    print(pairNames)
 
 if __name__ == '__main__':
     main()
