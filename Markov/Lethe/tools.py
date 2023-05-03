f5aef9b07035925e58aadd277d7096359d0fda7d
Côme Cattin <cattin.come@gmail.com>
2023-05-03 13:08:55 +0200

    Change the printing

--- Markov/Lethe/tools.py
+++ Markov/Lethe/tools.py
@@ -10,7 +10,7 @@ def create_pairIndices_from_pairNames(pdbfilename, pairNames):
     refu = mda.Universe(pdbfilename)
     pairsListIndices = []
     for pairname in pairNames:
-        print(f'search indices for the pair {pairname}')
+        print(f'Search indices for the pair {pairname}')
         atomName1 =  pairname.split('-')[0]
         res1 = atomName1.split('_')[0]
         name1 = atomName1.split('_')[1]
@@ -19,16 +19,19 @@ def create_pairIndices_from_pairNames(pdbfilename, pairNames):
         res2 = atomName2.split('_')[0]
         name2 = atomName2.split('_')[1]
         
-        print(name1, res1)
+        #print(name1, res1)
         index1 =   refu.select_atoms(f"name {name1} and resid {res1}").indices
-        print(name2, res2)
+        #print(name2, res2)
         index2 =   refu.select_atoms(f"name {name2} and resid {res2}").indices
         
         if len(index1) == 1 and len(index2) == 1 :
             pairsListIndices.append([index1[0],index2[0]])
+            
         else :
             print(f'WARNING : the pair defined by the name {pairname} do not lead to a pair on indices')
-            
+
+    print(f'Found indices: {pairsListIndices}')    
+    
     return pairsListIndices
 
 if __name__ == '__main__':
