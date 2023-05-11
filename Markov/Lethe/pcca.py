45208c00f747049d7ae3dab4200c03c7e73b830b
Côme Cattin <cattin.come@gmail.com>
2023-05-11 10:58:01 +0200

    Stationary probability

--- Markov/Lethe/pcca.py
+++ Markov/Lethe/pcca.py
@@ -9,7 +9,8 @@ from validation import *
 from markov_analysis import *
 
 def stationary_prob(msm,nstate):
-     msm.pca(nstate)
+     msm.pcca(nstate)
+     print('Sationary probabilities of the metastable sets')
      for i, s in enumerate(msm.metastable_sets):
           print('π_{} = {:f}'.format(i + 1, msm.pi[s].sum()))
 
@@ -42,6 +43,8 @@ if __name__ == '__main__':
     pdb = '/data/cloison/Simulations/HSP90-NT/SIMULATIONS_TRAJECTORIES/AMBER19SB_OPC/GS_cluster1.pdb'
     traj = ['/data/cloison/Simulations/HSP90-NT/SIMULATIONS_TRAJECTORIES/AMBER19SB_OPC/GS01_md_all_fitBB_protonly.xtc','/data/cloison/Simulations/HSP90-NT/SIMULATIONS_TRAJECTORIES/AMBER19SB_OPC/GS02_md_all_fitBB_protonly.xtc']
     outdir='/home/ccattin/Documents/Code/outputs'
+    filename='test.pyemma'
+    model_name = 'GS01_GS02'
     # Feat
     pairNames = ['64_CA-130_CA', '119_CA-24_CA']
     # Parameters
@@ -63,15 +66,28 @@ if __name__ == '__main__':
                           save=save,
                           display=display,
                           outdir=outdir)
-    cluster = clustering(reduction=tica,
-                         method='kmeans',
-                         k=200,
-                         stride=1)
     
+#     cluster = clustering(reduction=tica,
+#                          method='kmeans',
+#                          k=200,
+#                          stride=1)
+    
+#     msm = create_msm(cluster=cluster,
+#            lag=lag,
+#            error=False)
+    
+#     tools.save_model(
+#         cluster=cluster,
+#         msm=msm,
+#         outdir=outdir,
+#         model_name=model_name,
+#         filename=filename
+#         )
+    msm, cluster = tools.load_model(
+         outdir=outdir,
+         filename=filename,
+         model_name=model_name
+    )
 
-    msm = create_msm(cluster=cluster,
-           lag=lag,
-           error=False)
-    
     stationary_prob(msm=msm,
                     nstate=stable_state)
